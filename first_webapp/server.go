ca099b7969d560d637c1d5462cc36a6e4b987e5b
Tomoya Suzuki <forever.blue.moon@gmail.com>
2021-01-11 14:33:11 +0900

    add first_webapp

---
+++ first_webapp/server.go
@@ -0,0 +1,15 @@
+package first_webapp
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":6483", nil)
+}
